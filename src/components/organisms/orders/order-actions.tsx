708760f189ef3699d0072b8c0ceebb95d19c8a3e
s5sajid <sk8299148@gmail.com>
2025-11-21 19:33:09 +0500

    chore: basic formating

--- src/components/organisms/orders/order-actions.tsx
+++ src/components/organisms/orders/order-actions.tsx
@@ -2,11 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { updateOrderStatus } from "@/db/actions/dashboard/orders/actions";
 import { useAction } from "next-safe-action/hooks";
@@ -14,72 +14,72 @@ import { CheckCircle2, CircleX, Loader, Package, Truck } from "lucide-react";
 import { toast } from "sonner";
 
 interface OrderActionsProps {
-  orderId: string;
-  currentStatus:
-  | "PENDING"
-  | "PROCESSING"
-  | "SHIPPED"
-  | "DELIVERED"
-  | "CANCELLED";
+	orderId: string;
+	currentStatus:
+		| "PENDING"
+		| "PROCESSING"
+		| "SHIPPED"
+		| "DELIVERED"
+		| "CANCELLED";
 }
 
 export default function OrderActions({
-  orderId,
-  currentStatus,
+	orderId,
+	currentStatus,
 }: OrderActionsProps) {
-  const { execute, isExecuting } = useAction(updateOrderStatus, {
-    onSuccess: () => {
-      toast.success("Order status updated");
-    },
-    onError: ({ error }) => {
-      toast.error(error.serverError || "Failed to update status");
-    },
-  });
+	const { execute, isExecuting } = useAction(updateOrderStatus, {
+		onSuccess: () => {
+			toast.success("Order status updated");
+		},
+		onError: ({ error }) => {
+			toast.error(error.serverError || "Failed to update status");
+		},
+	});
 
-  const handleStatusUpdate = (
-    status: "PENDING" | "PROCESSING" | "SHIPPED" | "DELIVERED" | "CANCELLED"
-  ) => {
-    execute({ orderId, status });
-  };
+	const handleStatusUpdate = (
+		status: "PENDING" | "PROCESSING" | "SHIPPED" | "DELIVERED" | "CANCELLED"
+	) => {
+		execute({ orderId, status });
+	};
 
-  return (
-    <div className="flex items-center gap-2">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button disabled={isExecuting} variant="outline">
-            Update status
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Set status</DropdownMenuLabel>
-          <DropdownMenuItem
-            disabled={currentStatus === "PROCESSING"}
-            onClick={() => handleStatusUpdate("PROCESSING")}
-          >
-            <Loader className="mr-2 h-4 w-4 text-muted-foreground" /> Processing
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            disabled={currentStatus === "SHIPPED"}
-            onClick={() => handleStatusUpdate("SHIPPED")}
-          >
-            <Truck className="mr-2 h-4 w-4 text-blue-500" /> Fulfilled
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            disabled={currentStatus === "CANCELLED"}
-            onClick={() => handleStatusUpdate("CANCELLED")}
-          >
-            <CircleX className="mr-2 h-4 w-4 text-red-500" /> Cancelled
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      {currentStatus !== "DELIVERED" && (
-        <Button
-          disabled={isExecuting}
-          onClick={() => handleStatusUpdate("DELIVERED")}
-        >
-          <span>Mark Delivered</span>
-        </Button>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex items-center gap-2">
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button disabled={isExecuting} variant="outline">
+						Update status
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end">
+					<DropdownMenuLabel>Set status</DropdownMenuLabel>
+					<DropdownMenuItem
+						disabled={currentStatus === "PROCESSING"}
+						onClick={() => handleStatusUpdate("PROCESSING")}
+					>
+						<Loader className="mr-2 h-4 w-4 text-muted-foreground" /> Processing
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						disabled={currentStatus === "SHIPPED"}
+						onClick={() => handleStatusUpdate("SHIPPED")}
+					>
+						<Truck className="mr-2 h-4 w-4 text-blue-500" /> Fulfilled
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						disabled={currentStatus === "CANCELLED"}
+						onClick={() => handleStatusUpdate("CANCELLED")}
+					>
+						<CircleX className="mr-2 h-4 w-4 text-red-500" /> Cancelled
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+			{currentStatus !== "DELIVERED" && (
+				<Button
+					disabled={isExecuting}
+					onClick={() => handleStatusUpdate("DELIVERED")}
+				>
+					<span>Mark Delivered</span>
+				</Button>
+			)}
+		</div>
+	);
 }
